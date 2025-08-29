22a7065f8b07f6b2b7f95df7ec044f2ba6d333fc
Dhawal Mahajan <dhawal.mahajan1@gmail.com>
2025-08-29 18:56:57 +0530

    remove unwanted files

--- DhawalMahajanTask/Features/Presentation/Views/PortfolioSummaryView.swift
+++ DhawalMahajanTask/Features/Presentation/Views/PortfolioSummaryView.swift
@@ -74,7 +74,7 @@ final class PortfolioSummaryView: UIView {
     required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
     
     private func setUpUserInterface() {
-        backgroundColor = .systemBackground
+        backgroundColor = .lightText
         layer.cornerRadius = 14
         layer.masksToBounds = true
         addSubview(containerStack)
